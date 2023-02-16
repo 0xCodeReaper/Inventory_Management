7f917ee2e1771ffd70a71f0465d0ee48ab4ce1a1
0xCodeReaper <kapron.karol@gmail.com>
2023-02-16 13:23:33 +0000

    Improved OOP, added docstrings

--- inventory.py
+++ inventory.py
@@ -1,15 +1,31 @@
-# This program is a shoe inventory management system that allows the user to view, add, and modify the inventory of a shoe store. 
-# The program uses the tabulate library to display the inventory data in a tabular format, and stores the inventory data in 
-# a text file for persistence. The user can view all the shoes in the inventory, add new shoes, re-stock existing shoes, 
-# search for a shoe, calculate the value of each item, and see which product has the highest quantity available. 
-# The program utilizes object-oriented programming principles to represent each shoe as a Shoe object.
+"""A shoe inventory management system.
+
+This program allows the user to view, add, and modify the inventory of a shoe store.
+The program uses the tabulate library to display the inventory data in a tabular format, and stores the inventory data in 
+a text file for persistence. The user can view all the shoes in the inventory, add new shoes, re-stock existing shoes, 
+search for a shoe, calculate the value of each item, and see which product has the highest quantity available. 
+The program utilizes object-oriented programming principles to represent each shoe as a Shoe object.
+"""
 
 from tabulate import tabulate
 
+#=============Shoe list===========
+shoes = []
+"""Initialize the shoes list as a global variable."""
+
 #========The beginning of the class==========
 class Shoe:
+    """Represents a shoe.
 
+    Attributes:
+        country (str): The country of origin for the shoe.
+        code (str): The code of the shoe.
+        product (str): The name of the shoe.
+        cost (int): The cost of the shoe.
+        quantity (int): The quantity of the shoe in inventory.
+    """
     def __init__(self, country, code, product, cost, quantity):
+        """Initialize a new Shoe object with the given properties."""
         self.country = country
         self.code = code
         self.product = product
@@ -17,22 +33,21 @@ class Shoe:
         self.quantity = quantity
        
     def get_cost(self):
+        """Returns the cost of the shoe."""
         return self.cost
 
     def get_quantity(self):
+        """Returns the quantity of the shoe."""
         return self.quantity
-      
-    def __str__(self):
-        # Returns a string representation of the Shoe class
-        return f"{self.product} ({self.code}): {self.quantity} units available at ${self.cost} each from {self.country}."
-        
-#=============Shoe list===========
-shoes = []
 
+    def __str__(self):
+        """Returns a string representation of the Shoe object."""
+        return f"{self.product} ({self.code}): {self.quantity} units available at ${self.cost} each from {self.country}."
+       
 #==========Functions outside the class==============
 def read_shoes_data():
-    # This function opens the file inventory.txt and reads the data from this file, then creates a shoes object with this data
-    # and appends this object into the shoes list. 
+    """Reads shoe data from a file, populates the shoe list, and displays an error message if the inventory file cannot be found."""
+    global shoes
     try:
         with open("inventory.txt", "r") as f:
             next(f)  # Skip the first line
@@ -51,35 +66,41 @@ def read_shoes_data():
         print("An error occurred while reading the inventory file.")
 
 def capture_shoes():
-    # This function allows a user to capture data about a shoe and use this data to create a shoe object and append this object inside the shoe list.
+    """Captures data about a new shoe, adds it to the shoe list, and updates the inventory file with the new shoe information."""
+    global shoes
     country = input("Enter country of origin: ")
     code = input("Enter code of the shoe: ")
     product = input("Enter name of the shoe: ")
-    cost = float(input("Enter cost of the shoe: "))
+    cost = int(input("Enter cost of the shoe: "))
     quantity = int(input("Enter quantity of the shoe: "))
     shoe = Shoe(country, code, product, cost, quantity)
     shoes.append(shoe)
     print("Shoe added successfully!")
     with open("inventory.txt", "a") as f:
-        f.write(f"{country}, {code}, {product}, {cost}, {quantity}\n")
+        f.write(f"{country},{code},{product},{cost},{quantity}\n")
 
 def view_all():
-    # This function iterates over the shoes list and print the details of the shoes returned from the __str__ function
+    """Displays a table of all shoes in the inventory.
+
+    Uses the tabulate library to print a table with columns for shoe code, product name, country of origin, cost per item,
+    and quantity in inventory. The data is sourced from the global list of Shoe objects, and the table is sorted by
+    ascending shoe code."""
+    global shoes
     headers = ["Code", "Product", "Country", "Cost", "Quantity"]
     rows = [[shoe.code, shoe.product, shoe.country, shoe.cost, shoe.quantity] for shoe in shoes]
     print(tabulate(rows, headers=headers))
 
 def restock():
-    # This function will find the shoe object with the lowest quantity, which is the shoes that need to be re-stocked. Ask the user if they want to add this quantity of shoes and then update it.
-
+    """Finds the shoe object with the lowest quantity, asks the user if they want to add this quantity of shoes, and updates the inventory.""" 
+    global shoes
     # Find the shoe object with the lowest quantity using lambda
     min_quantity_shoe = min(shoes, key=lambda shoe: shoe.get_quantity())
     # Ask the user if they want to add this quantity of shoes
     print(f"The shoe with the lowest quantity is {min_quantity_shoe}.\n")
-    user_input = input("Do you want to add this quantity of shoes? (y/n): ")
+    add_shoe_quantity = input("Do you want to add this quantity of shoes? (y/n): ")
 
     # Update the quantity of the shoe object
-    if user_input.lower() == "y":
+    if add_shoe_quantity.lower() == "y":
         new_quantity = int(input(f"How many {min_quantity_shoe.product}s do you want to add?: "))
         min_quantity_shoe.quantity += new_quantity
         print(f"{new_quantity} {min_quantity_shoe.product}s added to the inventory.")
@@ -92,14 +113,15 @@ def restock():
                 if line.split(",")[1] == min_quantity_shoe.code:
                     line_data = line.split(",")
                     line_data[4] = str(int(line_data[4]) + new_quantity)
-                    line = ",".join(line_data)
+                    line = ",".join(line_data) + '\n'  # add newline character
                 f.write(line)
             f.truncate()
     else:
         print("No changes have been made to the inventory.")
 
 def search_shoe():
-    # This function searches for a shoe from the list using the shoe code and returns this object so that it will be printed.
+    global shoes
+    """Searches for a shoe from the list using the shoe code and returns this object so that it will be printed."""
     code = input("Enter the code of the shoe to search for: ")
     for shoe in shoes:
         if shoe.code == code:
@@ -108,7 +130,8 @@ def search_shoe():
     return None
 
 def value_per_item():
-    # This function calculates the total value for each item and prints this information on the console for all the shoes.
+    """Calculates the total value for each item and prints this information on the console for all the shoes."""
+    global shoes
     # Print a header for the shoe value table
     print("Shoe value per item:")
     print("---------------------")
@@ -119,7 +142,7 @@ def value_per_item():
         print(f"{shoe.product} ({shoe.code}): ${value:.2f}")
 
 def highest_qty():
-    # This code finds the product with the highest quantity and prints this shoe as being for sale.
+    """Finds the product with the highest quantity and prints this shoe as being for sale.""" 
     highest_qty_shoe = max(shoes, key=lambda shoe: shoe.get_quantity())
     print(f"{highest_qty_shoe.product} ({highest_qty_shoe.code}) is for sale with the highest quantity of {highest_qty_shoe.get_quantity()} pieces")
  
@@ -127,6 +150,8 @@ read_shoes_data()
 
 #==========Main Menu=============
 while True:
+    """Displays the main menu and prompts the user for input.
+    Based on the user's choice, the appropriate function is called."""
     print("===== Shoe Inventory Management =====")
     print("1. View all shoes")
     print("2. Add new shoe")
